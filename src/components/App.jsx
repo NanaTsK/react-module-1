a373b429cb7ad1e50fac7fc8ae00b2e05d546430
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-08 11:22:00 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import Header from './1-Header/Header';
+import Content from './2-Content/Content';
 
 export const App = () => {
   return (
@@ -12,11 +13,12 @@ export const App = () => {
         color: '#010101',
         marginTop: 20,
         marginLeft: 20,
-        textTransform: 'uppercase',
+        // textTransform: 'uppercase',
       }}
     >
-      react-module-1 :
+      <span style={{ textTransform: 'uppercase' }}>react-module-1 :</span>
       <Header />
+      <Content />
     </div>
   );
 };
--- src/components/index.styled.js
+++ src/components/index.styled.js
@@ -1,9 +1,9 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-  background-color: rgba(104, 133, 176, 0.3);
-  border-radius: 8px;
-  box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.7);
+  background-color: rgba(104, 133, 176, 0.2);
+  border-radius: 4px;
+  box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.5);
   color: #fff;
   margin: 10px auto;
   padding: 15px;
