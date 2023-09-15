c7e57f1083200730562db035e07b4850c303d5f5
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-15 23:57:27 +0200

    counter

--- src/components/3-Counter/Counter.jsx
+++ src/components/3-Counter/Counter.jsx
@@ -1,44 +1,91 @@
-// import React, { Component } from 'react';
+import { Component } from 'react';
 import { Container } from 'components/index.styled';
 
-// class Counter extends Component {
-//   render() {
-//     return (
-//       <Container>
-//         <div>
-//           <span>Counter</span>
-//         </div>
-//       </Container>
-//     );
-//   }
-// }
+class Counter extends Component {
+  state = {
+    counter: 0,
+    items: [],
+  };
 
-const Counter = () => {
-  return (
-    <Container>
-      <div>
-        <div className="card bg-dark text-white " style={{ width: '300px' }}>
-          <div className="card-body">
-            <h5 className="card-title text-center fs-1">Counter</h5>
-            <p className="card-text  text-center" style={{ fontSize: '80px' }}>
-              0
-            </p>
-            <div className="d-flex justify-content-center px-5">
-              <button
-                // onClick={handleClick}
-                className="btn btn-outline-success me-5"
+  handleClickIncrement = () =>
+    this.setState(prev => ({ counter: prev.counter + 1 }));
+
+  handleClickDecrement = () =>
+    this.setState(prev => ({ counter: prev.counter - 1 }));
+
+  //   handleClick = () => {
+  //     this.setState(prev => {
+  //       return { counter: prev.counter + 1 };
+  //     });
+  //   };
+
+  render() {
+    return (
+      <Container>
+        <div>
+          <div className="card bg-dark text-white " style={{ width: '300px' }}>
+            <div className="card-body">
+              <h5 className="card-title text-center fs-1">Counter</h5>
+              <p
+                className="card-text  text-center"
+                style={{ fontSize: '80px' }}
               >
-                <i className="bi bi-plus-circle fs-1"></i>
-              </button>
-              <button className="btn  btn-outline-danger ms-5">
-                <i className="bi bi-dash-circle fs-1"></i>
-              </button>
+                {this.state.counter}
+              </p>
+              <div className="d-flex justify-content-center px-5">
+                <button
+                  onClick={this.handleClickDecrement}
+                  className="btn  btn-outline-danger me-5"
+                >
+                  <i className="bi bi-dash-circle fs-1"></i>
+                </button>
+                <button
+                  onClick={this.handleClickIncrement}
+                  className="btn btn-outline-success ms-5"
+                >
+                  <i className="bi bi-plus-circle fs-1"></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </Container>
-  );
-};
+      </Container>
+    );
+  }
+}
+
+// const Counter = () => {
+//   let counter = 0;
+
+//   const handleClick = e => {
+//     counter++;
+//     console.log(counter);
+//   };
+//   return (
+//     <Container>
+//       <div>
+//         <div className="card bg-dark text-white " style={{ width: '300px' }}>
+//           <div className="card-body">
+//             <h5 className="card-title text-center fs-1">Counter</h5>
+//             <p className="card-text  text-center" style={{ fontSize: '80px' }}>
+//               {counter}
+//             </p>
+//             <div className="d-flex justify-content-center px-5">
+//               <button className="btn  btn-outline-danger me-5">
+//                 <i className="bi bi-dash-circle fs-1"></i>
+//               </button>
+//               <button
+//                 onClick={handleClick}
+//                 className="btn btn-outline-success ms-5"
+//               >
+//                 <i className="bi bi-plus-circle fs-1"></i>
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </Container>
+//   );
+// };
 
 export default Counter;
