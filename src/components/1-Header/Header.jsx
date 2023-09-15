2e1c7441d971a6266ad18a37f0229679dc9206b5
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-15 23:58:05 +0200

    header + modal

--- src/components/1-Header/Header.jsx
+++ src/components/1-Header/Header.jsx
@@ -1,16 +1,20 @@
 import { Container } from 'components/index.styled';
-import { TypographyHeader, TypographyText } from './Header.styled';
+// import { TypographyHeader, TypographyText } from './Header.styled';
 
 const Header = () => {
-  const isOnline = true;
+  // const isOnline = true;
   return (
     <Container>
-      <div>
-        <TypographyHeader $isOnline={isOnline}>Hello React</TypographyHeader>
-        <TypographyText>something</TypographyText>
-      </div>
+      <nav className="navbar bg-dark mb-3">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 text-success">Navbar</span>
+          <button className="btn btn-outline-success">Open Modal</button>
+        </div>
+      </nav>
     </Container>
   );
 };
 
 export default Header;
+
+/* <button onClick={toggleModal} className="btn btn-outline-success"> */
--- src/components/1-Header/Header.styled.js
+++ src/components/1-Header/Header.styled.js
@@ -10,23 +10,23 @@ import { styled } from 'styled-components';
 //   };
 // });
 
-export const TypographyHeader = styled.h1`
-  // color: teal;
-  margin: 0;
-  color: ${({ isOnline }) => (isOnline ? 'teal' : 'black')};
-`;
-// const color = '#000';
-// const TypographyHeader = styled('h1')({
-//   boxShadow: ({ isOnline }) => `0 0 12px 12px ${isOnline ? color : '#00fbff'}`,
+// export const TypographyHeader = styled.h1`
+//   // color: teal;
+//   margin: 0;
+//   color: ${({ isOnline }) => (isOnline ? 'teal' : 'black')};
+// `;
+// // const color = '#000';
+// // const TypographyHeader = styled('h1')({
+// //   boxShadow: ({ isOnline }) => `0 0 12px 12px ${isOnline ? color : '#00fbff'}`,
+// // });
+
+// // export const NewTypographyHeader = styled(TypographyHeader)({
+
+// // })
+
+// export const TypographyText = styled('p')(({ isOnline }) => {
+//   return {
+//     color: isOnline ? 'black' : 'red',
+//     textAlign: isOnline ? 'center' : 'right',
+//   };
 // });
-
-// export const NewTypographyHeader = styled(TypographyHeader)({
-
-// })
-
-export const TypographyText = styled('p')(({ isOnline }) => {
-  return {
-    color: isOnline ? 'black' : 'red',
-    textAlign: isOnline ? 'center' : 'right',
-  };
-});
