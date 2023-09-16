5448284eaf420bfd320a2f8a0bb1160da777d219
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-16 10:20:39 +0200

    modal

--- src/components/1.1-Modal/Modal.jsx
+++ src/components/1.1-Modal/Modal.jsx
@@ -1,33 +1,9 @@
-// const Modal = ({ children, toggleModal }) => {
-//   return (
-//     <div
-//       className="modal fade show"
-//       style={{ display: 'block', backdropFilter: 'blur(5px)' }}
-//       onClick={toggleModal}
-//     >
-//       <div className="modal-dialog">
-//         <div className="modal-content">
-//           <div className="modal-header">
-//             <h5 className="modal-title"> Modal</h5>
-//             <button
-//               type="button"
-//               className="btn-close"
-//               aria-label="Close"
-//               onClick={toggleModal}
-//             ></button>
-//           </div>
-//           <div className="modal-body">{children}</div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-const Modal = ({ children }) => {
+const Modal = ({ children, toggleModal }) => {
   return (
     <div
       className="modal fade show"
       style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+      //   onClick={toggleModal}
     >
       <div className="modal-dialog">
         <div className="modal-content">
@@ -37,7 +13,7 @@ const Modal = ({ children }) => {
               type="button"
               className="btn-close"
               aria-label="Close"
-              onClick={this.toggleModal}
+              onClick={toggleModal}
             ></button>
           </div>
           <div className="modal-body">{children}</div>
